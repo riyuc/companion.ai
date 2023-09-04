42f8820b2e2a52d5a053e3631d5c83f0ad548546
Liyuchi <bongbang0101@gmail.com>
2023-09-04 19:26:57 -0400

    configure fancy multi select

--- components/ui/fancy-multi-select.tsx
+++ components/ui/fancy-multi-select.tsx
@@ -82,7 +82,7 @@ export function FancyMultiSelect() {
   return (
     <Command onKeyDown={handleKeyDown} className="overflow-visible bg-transparent">
       <div
-        className="group border border-input px-3 py-2 text-sm ring-offset-background rounded-md focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2"
+        className=" bg-black group border border-input px-3 py-2 text-sm ring-offset-background rounded-md focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2"
       >
         <div className="flex gap-1 flex-wrap">
           {selected.map((framework) => {
